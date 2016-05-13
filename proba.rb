8b8fa232d730faf6ec6666dca62eafb40d921eba
gorgolevsky <pgorgolevskiy@npoprogress.com>
2016-05-13 19:02:17 +0300

    checking enough cards in the deck and some refactoring

--- proba.rb
+++ proba.rb
@@ -14,79 +14,97 @@ require_relative 'dealer'
 require_relative 'card_deck'
 require_relative 'card'
 
+# поставить гем flash и передавать сообщение во флэше
 # по окончании игры - флашить данные в редисе (поставить stale время)
 # добавить проверки на закончившиеся деньги у игрока
 # безопасность - не давать пользователю отправить запрос уже после выигрыша и до ресета (кнопки на вьюхе задизэйблены, но урл он может вбить вручную)
 
 get '/' do
   # Первоначальный вход
-  reset_game
-  player.reset_money
+  player.reset_money # if ...
 
   slim :start
+end
+
+post '/start_game_set' do
+  reset_game
+  slim :set_stake
+end
+
+post '/start_round' do
+  unless params['stake'] && params['stake'].to_i > 0
+    # здесь проверка на достаточное количество денег у игрока
+    redirect '/set_stake'
+  end
+
+  set_stake
+  get_initial_cards
+  check_win_conditions
+
+  slim :game
 end
 
 get '/set_stake' do
   slim :set_stake
 end
 
-get '/game' do
-  set_variables_check_win_conditions
-
-  slim :game
-end
-
-post '/start' do
-  unless params['stake'] && params['stake'].to_i > 0
-    # поставить гем flash и передавать сообщение во флэше
-    redirect '/set_stake'
+post '/double_stake' do
+  if player.money >= player.stake*2
+    player.double_stake
+    @stake_doubled = :doubled
+  else
+    @stake_doubled = :not_doubled
   end
 
-  set_stake
-  get_initial_cards
-
-  set_variables_check_win_conditions
+  set_variables
   slim :game
 end
 
-post '/double_stake' do  #(в будущем сделать Ajax'ом)
-  player.double_stake
-  dealer
-  @stake_doubled = true
-  slim :game
-end
+post '/hit_me' do
+  check_enough_cards_left
 
-post '/hit_me' do  #(в будущем сделать Ajax'ом)
-  # здесь тоже сделать проверку на количество карт
   player.give_cards 1
+  set_variables
+  check_win_conditions
 
-  set_variables_check_win_conditions
   slim :game
 end
 
 post '/stand' do
-  # здесь тоже сделать проверку на количество карт
+  check_enough_cards_left
+
   player.stand = 1
   dealer.get_cards_to_score_17
 
-  set_variables_check_win_conditions
+  check_win_conditions
   slim :game
 end
 
 post '/another_round' do
-  if card_deck.not_enough_cards?
-    slim :no_cards
-  else
-    reset_round
-    slim :set_stake
-  end
+  # здесь тоже нужно проверить деньги игрока и не давать начать новый раунд с сообщением причины на странице
+
+  check_enough_cards_left
+
+  reset_round
+  slim :set_stake
+end
+
+get '/no_cards' do
+  slim :no_cards
 end
 
 private
 
-def set_variables_check_win_conditions
+def check_enough_cards_left
+  redirect '/no_cards' if card_deck.not_enough_cards?
+end
+
+def set_variables
   player; dealer
-  @player.stand? ? check_2nd_win_condition : check_1st_win_condition
+end
+
+def check_win_conditions
+  player.stand? ? check_2nd_win_condition : check_1st_win_condition
 end
 
 def reset_game
